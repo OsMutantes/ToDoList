b1e67aa3729a4f5175393bf6cdf14bc28a5a57af
Amit Cesar <amitcesar27@gmail.com>
2025-03-11 23:52:56 -0300

    feat: add colors

---
+++ composeApp/src/commonMain/kotlin/com/github/mutantes/style/Colors.kt
@@ -0,0 +1,22 @@
+package com.github.mutantes.style
+import androidx.compose.ui.graphics.Color
+
+
+
+
+object Colors {
+    val blueLight = Color(0xFF4EA8DE)
+    val blueDark = Color(0xFF1E6F9F)
+    val purpleLight = Color(0xFF5E60CE)
+    val purpleBright = Color(0xFF8284FA)
+
+    val gray700 = Color(0xFF0D0D0D) // Cinza mais escuro
+    val gray600 = Color(0xFF1A1A1A)
+    val gray500 = Color(0xFF262626)
+    val gray400 = Color(0xFF333333)
+    val gray300 = Color(0xFF808080)
+    val gray200 = Color(0xFFD9D9D9)
+    val gray100 = Color(0xFFF2F2F2)
+    val feedbackColor = Color(0xE25858)
+}
+
