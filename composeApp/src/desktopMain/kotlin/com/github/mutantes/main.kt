65cab8d409b989d28c6d63925f945a69f046e542
italomelo <italord.melo@gmail.com>
2025-03-11 23:03:46 -0300

    Changed window size for desktop

--- composeApp/src/desktopMain/kotlin/com/github/mutantes/main.kt
+++ composeApp/src/desktopMain/kotlin/com/github/mutantes/main.kt
@@ -1,12 +1,20 @@
 package com.github.mutantes
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+
+    val state = rememberWindowState(
+        height = 1280.dp,
+        width = 720.dp
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "ToDoApp",
+        state = state
     ) {
         App()
     }
